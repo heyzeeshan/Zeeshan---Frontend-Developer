48cd4b5de0aa1b72cd4e9fb31d09515d66f4903c
Mohammed Zeeshan <mzjagirdar10@gmail.com>
2023-03-18 19:27:15 +0530

    [Add: Test Script for banner and fetch API]

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
+import Rockets from './components/pages/Rocket'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders spacex message', () => {
+  const { getByText } = render(<App />);
+  const welcomeMessage = getByText('SpaceX');
+  expect(welcomeMessage).toBeInTheDocument();
+});
+
+test('fetches data from SpaceX API', async () => {
+  const response = await fetch('https://api.spacexdata.com/v4/rockets');
+  const data = await response.json();
+  expect(response.status).toEqual(200);
+  expect(data.length).toBeGreaterThan(0);
 });
